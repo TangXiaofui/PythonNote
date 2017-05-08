aba12c7423253c184b412a440c92b35b9c16ac85
TangXiaofui <1246884224@qq.com>
2017-05-08 19:30:17 +0800

    functional programming

--- functionalProgram.py
+++ functionalProgram.py
@@ -57,3 +57,82 @@ L=['bob', 'about', 'Zoo', 'Credit']
 print(sorted(L))
 print(sorted(L,key = str.lower))
 print(sorted(L,key = str.lower,reverse=True))
+
+
+def lazy_sum(*args):
+    def sum():
+        total = 0
+        for i in args:
+            total += i
+        return total
+    return sum
+
+sum1 = lazy_sum(1,2,3,4)
+sum2 = lazy_sum(1,2,3,4)
+print (sum1 == sum2)
+print (sum1())
+
+
+def count():
+    fs = []
+    for i in range(1,4):
+        def f():
+            return i*i
+        fs.append(f)
+    return fs
+f1,f2,f3= count()
+print(f1())
+print(f2())
+print(f3())
+
+
+def count2():
+    fs = []
+    def f(i):
+        def g():
+            return i*i
+        return g
+    for i in range(1,4):
+        fs.append(f(i))
+    return fs
+f1,f2,f3= count2()
+print(f1())
+print(f2())
+print(f3())
+
+
+m1 = map(lambda x:x*x ,[1,2,3,4,5,6,7,8])
+f1 = lambda x:x*x
+print (list(m1))
+print (f1(5))
+
+import functools
+
+
+def log(text):
+    def deractor(func):
+        @functools.wraps(func)
+        def Wrapper(*args,**kw):
+            print("%s %s"%(text,func.__name__))
+            return func(*args,**kw)
+        return Wrapper
+    return deractor
+
+#@log('execute')
+def now():
+    print("2017-5-8")
+now = log('execute')(now)
+print(now())
+print(now.__name__)
+
+
+def int2(x,base=2):
+    return int(x,base)
+def int16(x,base=16):
+    return int(x,base)
+print (int2('10101010'))
+print (int16('0x10'))
+
+
+int8=functools.partial(int,base=8)
+print(int8('010'))
